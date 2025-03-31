bb04419175a78dbbcfeb56aaa67a9d9bb99f77e6
Ishita Jindal <ishijindal1310@gmail.com>
2025-03-31 19:50:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(cors());
 app.use(
 	cors({
-		origin: "http://127.0.0.1:8000",
+		origin: "http://127.0.0.1:5500",
 		methods: ["POST"],
 		optionsSuccessStatus: 200,
 		credentials: true,
