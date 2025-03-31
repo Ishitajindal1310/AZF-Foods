9215fd3994c765229c7332c60a69b27a4eee9cfd
Ishita Jindal <ishijindal1310@gmail.com>
2025-03-31 19:56:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(cors());
 app.use(
 	cors({
-		origin: "https://azf-foods-5yq3.vercel.app/",
+		origin: "https://azf-foods-5yq3.vercel.app",
 		methods: ["POST"],
 		optionsSuccessStatus: 200,
 		credentials: true,
