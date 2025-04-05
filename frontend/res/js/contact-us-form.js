d221fcf4f2f81880961c2f53ddb423bcfaad2a60
Ishita Jindal <ishijindal1310@gmail.com>
2025-04-05 21:35:44 +0530

    Update contact-us-form.js

--- frontend/res/js/contact-us-form.js
+++ frontend/res/js/contact-us-form.js
@@ -2,16 +2,17 @@ document.getElementById("contactForm").addEventListener("submit", async (e) => {
 	e.preventDefault();
 
 	const formData = {
-		name: document.getElementById("name").value,
+		fName: document.getElementById("fName").value,
+		lName: document.getElementById("lName").value,
 		phone: document.getElementById("phone").value,
-		date: document.getElementById("date").value,
-		time: document.getElementById("time").value,
+		email: document.getElementById("email").value,
 		product: document.getElementById("product").value,
 		location: document.getElementById("Location").value,
 		notes: document.getElementById("notes").value,
 	};
 	try {
 		const response = await fetch("https://azf-foods.vercel.app/submit-form", {
+			// const response = await fetch("http://127.0.0.1:8000/submit-form", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -22,7 +23,6 @@ document.getElementById("contactForm").addEventListener("submit", async (e) => {
 
 		if (response.ok) {
 			alert("Form submitted successfully!");
-			// this.reset();
 		} else {
 			alert(`Error: ${result.error || "Failed to submit form"}`);
 		}
